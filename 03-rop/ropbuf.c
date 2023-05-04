83ad14a5043514d7b79c70bf2e7b74105ce8092b
insa cvl <aabda2000@gmail.com>
2023-05-04 16:37:19 +0200

    03-rop

---
+++ 03-rop/ropbuf.c
@@ -0,0 +1,20 @@
+/* Disable ASLR: echo 0 | sudo tee /proc/sys/kernel/randomize_va_space   */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void vuln(char *input)
+{
+    char buf[128];
+    strcpy(buf, input);
+}
+
+int main(int argc, char **argv)
+{
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stdin, NULL, _IONBF, 0);
+    if (argc > 1)
+        vuln(argv[1]);
+    return 0;
+}
