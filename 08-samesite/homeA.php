a263ab300efe84d27f57b884928bbdc5aa0add5f
insa cvl <aabda2000@gmail.com>
2023-05-24 15:26:29 +0200

    add web pages for samesite

--- 08-samesite/homeA.php
+++ 08-samesite/homeA.php
@@ -1,16 +1,21 @@
 <?php
 //session name
 session_name('sessionCookie');
-session_set_cookie_params(['lifetime' => 0, 'path' => '/', 'domain' => '.asterix',  'httponly' => true, 'samesite' => 'strict']);
+session_set_cookie_params(['lifetime' => 0, 'path' => '/', 'domain' => '.asterix',  'httponly' => true, 'samesite' => 'lax']);
 session_save_path($_SERVER['DOCUMENT_ROOT'] . '/sessions');
 session_start();
 
 //session stuff
 $_SESSION['logged_in'] = 'worked';
-$_SESSION['count'] = 1;
+$_SESSION['lax_count'] = 0;
+$_SESSION['strict_count'] = 0;
+$_SESSION['none_count'] = 0;
 //cookie stuff
-setcookie('myCookie', 'worked', ['expires' => 0, 'path' => '/', 'domain' => 'asterix',  'httponly' => true, 'samesite' => 'lax']);
+
+setcookie('myStrictCookie', 'strict_worked', ['expires' => 0, 'path' => '/', 'domain' => 'asterix',  'httponly' => true, 'samesite' => 'strict']);
+setcookie('myLaxCookie', 'lax_worked', ['expires' => 0, 'path' => '/', 'domain' => 'asterix',  'httponly' => true, 'samesite' => 'lax']);
+setcookie('myNoneCookie', 'none_worked', ['expires' => 0, 'path' => '/', 'domain' => 'asterix',  'httponly' => true, 'samesite' => 'none']);
 
 //redirect to the other page
-header("Location: /somepage.php");
+header("Location: /somepageA.php");
 exit();
--- 08-samesite/somepageA.php
+++ 08-samesite/somepageA.php
@@ -5,12 +5,18 @@ session_name('sessionCookie');
 session_set_cookie_params(['lifetime' => 0, 'path' => '/', 'domain' => '.asterix', 'httponly' => true, 'samesite' => 'lax']);
 session_save_path($_SERVER['DOCUMENT_ROOT'] . '/sessions');
 session_start();
-if ($_COOKIE['myCookie'] == 'worked') {
-    $_SESSION['count'] = $_SESSION['count'] + 1;
+if ($_COOKIE['myLaxCookie'] == 'lax_worked') {
+    $_SESSION['lax_count'] = $_SESSION['lax_count'] + 1;
 };
 
 
+if ($_COOKIE['myStrictCookie'] == 'strict_worked') {
+    $_SESSION['strict_count'] = $_SESSION['strict_count'] + 1;
+};
 
+if ($_COOKIE['myNoneCookie'] == 'none_worked') {
+    $_SESSION['none_count'] = $_SESSION['none_count'] + 1;
+};
 
 echo '<pre>' . print_r($_SESSION, 1) . '</pre>';
 
