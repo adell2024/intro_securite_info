f770d14aedf691116804673f229fe124b3ab37be
aabda2000 <38082725+aabda2000@users.noreply.github.com>
2023-05-25 23:45:33 +0200

    Update homeA.php

--- 08-samesite/homeA.php
+++ 08-samesite/homeA.php
@@ -1,7 +1,7 @@
 <?php
 //session name
 session_name('sessionCookie');
-session_set_cookie_params(['lifetime' => 0, 'path' => '/', 'domain' => '.asterix',  'httponly' => true, 'samesite' => 'lax']);
+session_set_cookie_params(['lifetime' => 0, 'path' => '/', 'domain' => '.mabanque.fr',  'httponly' => true, 'samesite' => 'lax']);
 //session_save_path($_SERVER['DOCUMENT_ROOT'] . '/sessions');
 session_start();
 
@@ -12,9 +12,9 @@ $_SESSION['strict_count'] = 0;
 $_SESSION['none_count'] = 0;
 //cookie stuff
 
-setcookie('myStrictCookie', 'strict_worked', ['expires' => 0, 'path' => '/', 'domain' => 'asterix',  'httponly' => true, 'samesite' => 'strict']);
-setcookie('myLaxCookie', 'lax_worked', ['expires' => 0, 'path' => '/', 'domain' => 'asterix',  'httponly' => true, 'samesite' => 'lax']);
-setcookie('myNoneCookie', 'none_worked', ['expires' => 0, 'path' => '/', 'domain' => 'asterix',  'httponly' => true, 'samesite' => 'none']);
+setcookie('myStrictCookie', 'strict_worked', ['expires' => 0, 'path' => '/', 'domain' => '.mabanque.fr',  'httponly' => true, 'samesite' => 'strict']);
+setcookie('myLaxCookie', 'lax_worked', ['expires' => 0, 'path' => '/', 'domain' => '.mabanque.fr',  'httponly' => true, 'samesite' => 'lax']);
+setcookie('myNoneCookie', 'none_worked', ['expires' => 0, 'path' => '/', 'domain' => '.mabanque.fr',  'httponly' => true, 'samesite' => 'none']);
 
 //redirect to the other page
 header("Location: /somepageA.php");
