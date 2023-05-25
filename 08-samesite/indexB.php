f939cc49a0d85c85b9934d2c884dd0dac2dc5657
aabda2000 <38082725+aabda2000@users.noreply.github.com>
2023-05-25 23:46:38 +0200

    Update indexB.php

--- 08-samesite/indexB.php
+++ 08-samesite/indexB.php
@@ -9,7 +9,11 @@
 
 <body>
     <h1> cette page appartient au site domaineB.fr </h1>
-    <a href='http://asterix:3000/somepageA.php'>asterix Site</a>
+    <a href='http://mabanque.fr/somepageA.php'>mabanque</a>
+
+    <form metho="post" action="http://mabanque.fr/somepageA.php">
+        post method <input type="submit">
+    </form>
 </body>
 
 </html>
