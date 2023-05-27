0bbfe4f6c16d3b5398921b36d4eb24baf8f72fd6
aabda2000 <38082725+aabda2000@users.noreply.github.com>
2023-05-27 22:30:54 +0200

    Update login.php

--- 09-csp/02-comprehend-csp/login.php
+++ 09-csp/02-comprehend-csp/login.php
@@ -7,12 +7,12 @@ session_start(); ?>
 <form id="register_form" method="POST" action="welcome.php">
     <h1>Register</h1>
     <input type="text" id="user_name" name="user_name" />
-    <input type="password" id="password" name="password" onkeyup="javascript:check()" />
+    <input type="password" id="password" name="password" onkeyup="check()" />
     <input type="submit" value="Register" />
 </form>
 
 <script>
-    //document.getElementById("register_form").onsubmit = check;
+    //document.getElementById("password").onkeyup = check;
 
     function check() {
         var password = document.getElementById("password").value;
