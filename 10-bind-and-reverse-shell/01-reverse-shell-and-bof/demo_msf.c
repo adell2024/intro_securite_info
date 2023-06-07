45be2040186b768286e2ba925d8995545b4aa3a2
insa cvl <aabda2000@gmail.com>
2023-06-07 14:17:21 +0200

    demo msf c program

---
+++ 10-bind-and-reverse-shell/01-reverse-shell-and-bof/demo_msf.c
@@ -0,0 +1,21 @@
+#include <stdio.h
+
+#include <unistd.h>
+
+/* Compile: gcc -fno-stack-protector -z execstack demo_msf.c -o demo_msf -g */
+
+int vuln(){
+ char buffer[600];
+ int characters_read;
+ //printf("bufer =%p\n",buffer);
+ printf("Enter message:\n");
+ characters_read = read(0, buffer, 1000);
+ printf("You entered: %s", buffer);
+ return 0;
+
+}
+
+void main(){
+  vuln();
+    //atterissage après retour de vuln  
+}
