ae61de734f1db273c8e8be0dd79bd70e884f9fd0
adell2024 <159798073+adell2024@users.noreply.github.com>
2025-06-07 12:33:42 +0200

    Update hfdo.php

--- 06-sop-cors-csp/03-sop-cors/hfdo.php
+++ 06-sop-cors-csp/03-sop-cors/hfdo.php
@@ -1,23 +1,23 @@
 <?php
 // Définir l'origine autorisée
-$allowedOrigin = "http://192.168.180.136:5000";
+$allowedOrigin = "http://192.168.209.129:5000";
 
-if (isset($_SERVER["HTTP_ORIGIN"]) && $_SERVER["HTTP_ORIGIN"] === $allowedOrigin) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-} else {
-    header("Access-Control-Allow-Origin: $allowedOrigin");
-}
 
+// Définir les en-têtes CORS
+header("Access-Control-Allow-Origin: $allowedOrigin");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Max-Age: 60");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Header-Custom-TizenCORS");
+header("Content-Type: application/json");
 
+// Gérer la requête OPTIONS
 if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
-    exit(0); // Répondre immédiatement pour les requêtes OPTIONS
+    http_response_code(200);
+    exit(0);
 }
 
-header('Content-Type: application/json');
+// Réponse pour la requête GET
 $doctor = array(
     'id' => 1,
     'name' => "Folamour",
